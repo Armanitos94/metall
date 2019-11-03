d8fa68714e455cabc8706e521ef199d377939c37
Arman Shahnazaryan <as@delitec.ru>
2019-11-03 20:17:00 +0400

    add google sheets

---
+++ js/loadDataFromGoogleSheets.js
@@ -0,0 +1,56 @@
+(function($) {
+  "use strict";
+
+  var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1BlI5fM_kmfPrXh_9j0OLyrA9zPWVxJaLnc9JlaYmivc/pubhtml';
+
+  // Builds the HTML Table out of collection.
+  function buildHtmlTable(collection, selector) {
+    var columns = addAllColumnHeaders(collection, selector);
+
+    for (var i = 0; i < collection.length; i++) {
+      var row$ = $('<tr/>');
+      for (var colIndex = 0; colIndex < columns.length; colIndex++) {
+        var cellValue = collection[i][columns[colIndex]];
+        if (cellValue == null) cellValue = "";
+        row$.append($('<td/>').html(cellValue));
+      }
+      $(selector).append(row$);
+    }
+  }
+
+  // Adds a header row to the table and returns the set of columns.
+  // Need to do union of keys from all records as some records may not contain
+  // all records.
+  function addAllColumnHeaders(collection, selector) {
+    var columnSet = [];
+    var headerTr$ = $('<tr/>');
+
+    for (var i = 0; i < collection.length; i++) {
+      var rowHash = collection[i];
+      for (var key in rowHash) {
+        if ($.inArray(key, columnSet) == -1) {
+          columnSet.push(key);
+          headerTr$.append($('<th/>').html(key));
+        }
+      }
+    }
+    $(selector).append(headerTr$);
+
+    return columnSet;
+  }
+
+  function init() {
+    Tabletop.init( { key: publicSpreadsheetUrl,
+                     callback: showInfo,
+                     simpleSheet: false } )
+  }
+
+  function showInfo(data, tabletop) {
+    buildHtmlTable(data[Object.keys(data)[0]].elements, "#blackMetal");
+    buildHtmlTable(data[Object.keys(data)[1]].elements, "#otherMetal");
+
+  }
+
+  window.addEventListener('DOMContentLoaded', init)
+
+})(jQuery);
